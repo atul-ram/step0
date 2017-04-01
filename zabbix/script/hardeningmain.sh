689a3fb95f6f375ee78a472f63071d2a8d05aed1
atul sahu <atul.ram@gmail.com>
2017-04-01 20:22:41 +0530

    ignore commit

--- zabbix/script/hardeningmain.sh
+++
@@ -1,3 +0,0 @@
-ip=`echo "$1"`
-harden=`/bin/sh /etc/zabbix/script/hardening.sh`
-/usr/local/sbin/zabbix_sender -z $ip -s `hostname`  -k HARDENDATA -o "$harden"
--- zabbix/script/userdetails.sh
+++
@@ -1,3 +0,0 @@
-ip=`echo "$1"`
-userdetail=`cat /etc/passwd |grep -v "#"|grep  home |awk -F: '{print $1,$5}'`
-/usr/local/sbin/zabbix_sender -z $ip -s `hostname` -k USERDETAILS -o "$userdetail"
