1920296cd029e4305c47b12702af3c4b6f9bc855
atul sahu <atul.ram@gmail.com>
2017-04-02 11:37:02 +0530

    final

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+# set -o xtrace
+set -o pipefail
+
+__DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
+__BASE__="$(basename "${0}")"
+__FILE__="${__DIR__}/${__BASE__}"
+
+echo "################################"
+echo "Build Information"
+echo "Directory: ${__DIR__}"
+echo "Filename: ${__FILE__}"
+echo "################################"
+
+echo "### running playbook"
+
+ansible-playbook  --extra-vars="target=$1" example2.yml
+
+
